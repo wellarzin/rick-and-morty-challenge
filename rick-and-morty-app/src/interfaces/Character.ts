04f63948d5782772cc53dd9a80586c32669e26d0
wellarzin <brunodiaswwll@gmail.com>
2024-09-08 14:56:11 -0300

    feat: creating interface Character

---
+++ rick-and-morty-app/src/interfaces/Character.ts
@@ -0,0 +1,10 @@
+export default interface Character {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    image: string;
+    gender: string;
+    type: string;
+  }
+
